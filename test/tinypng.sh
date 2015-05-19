f6c5de098b583cd42297f0259203fc77dded378a
Yang Li <liyangyijie@users.noreply.github.com>
2015-05-19 20:56:23 +0800

    Update tinypng.sh

--- test/tinypng.sh
+++ test/tinypng.sh
@@ -1,29 +1,41 @@
 #!/bin/bash
-
 ###############
 #vars begin
 API=""
 DOC_NAME="Optimized"
 #vars end
 ###############
-
 ###############
 #main begin
+function N_K(){
+    local IFS='|'
+    for fn in $KG
+    do
+        newfn=`echo $fn|sed 'y/ /-/'|sed 's/^-*//'`
+        echo "From $(echo $fn|sed 's/ /\\ /g') to $newfn"
+        mv $(echo $fn|sed 's/ /\\ /g') $newfn
+    done
+}
+function UP_FILE(){
+    AllFiles=$(ls|egrep '.*\.(png|jpg|jpeg)$')
+    for FileName in ${AllFiles}
+    do
+    echo "Optimize ${FileName}"
+    JSON=`curl -si --user api:${API} --data-binary @${FileName} https://api.tinypng.com/shrink`
+    FileAdd=`echo $JSON|sed -n 's/.*"url":"\([^"]*\).*/\1/p'`
+    [ "$FileAdd" = "" ] && echo "Optimize ${FileName} failure!"
+    [ "$FileAdd" != "" ] && {
+        wget $FileAdd --no-check-certificate -qO ${Front}/${FileName}
+        echo "You could get Optimized version From ${Front}/${FileName}"
+    }
+    done
+}
 Script_Dir="$(cd "$(dirname $0)"; pwd)"
 Front="${Script_Dir}/${DOC_NAME}"
 [ ! -d $Front ] && mkdir $Front
-AllFiles=`ls|egrep '.*\.(png|jpg|jpeg)$'`
-for FileName in ${AllFiles}
-do
-echo "Optimize ${FileName}"
-JSON=`curl -si --user api:${API} --data-binary @${FileName} https://api.tinypng.com/shrink`
-FileAdd=`echo $JSON|sed -n 's/.*"url":"\([^"]*\).*/\1/p'`
-[ "$FileAdd" = "" ] && echo "Optimize ${FileName} failure!"
-[ "$FileAdd" != "" ] && {
-wget $FileAdd --no-check-certificate -qO ${Front}/${FileName}
-echo "You could get Optimized version From ${Front}/${FileName}"
-}
-done
+KG=$(ls|egrep '.*\.(png|jpg|jpeg)$'|grep ' '|sed ':a;N;s/\n/|/;ba;')
+[ "$KG" != "" ] && N_K
+UP_FILE
 #main end
 ###############
 exit 0
