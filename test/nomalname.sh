9a82066d6ed84d305cad23a5c86736a3b1f07a63
Yang Li <liyangyijie@users.noreply.github.com>
2015-05-19 21:17:25 +0800

    Update nomalname.sh

--- test/nomalname.sh
+++ test/nomalname.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-allfilename=`echo *`
+allfilename=`ls -F|grep -v '/$'|sed ':a;N;s/\n/|/;ba;'`
+IFS='|'
 for fn in $allfilename
 do
-    newfn=`echo $fn|sed 'y/()[]「」 （）　"“”/-------------/'|sed 's/^-//'`
+    newfn=`echo $fn|sed 'y/()[]「」 （）　"“”/-------------/'|sed 's/^-*//'`
     echo "From $fn to $newfn"
-    mv $fn $newfn
+    mv "$fn" "$newfn"
 done
