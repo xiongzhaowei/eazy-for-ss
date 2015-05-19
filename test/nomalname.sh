eeb026e9ea39152470c8acd45299b35fe5028408
Yang Li <liyangyijie@users.noreply.github.com>
2015-05-19 22:17:55 +0800

    Update nomalname.sh

--- test/nomalname.sh
+++ test/nomalname.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 function N_K(){
-IFS="$1"
+local IFS="$1"
 for fn in $allfilename
 do
-    newfn=`echo $fn|sed 'y/()[]「」 （）　"“”:|?/----------------/'|sed 's/^-*//'`
-    echo "From $fn to $newfn"
+    newfn=`echo $fn|sed 'y/()[]「」 （）　"“”:|?-/_________________/'`
+    echo "From $(echo $fn|sed 's/ /\\ /g') to $newfn"
     mv "$fn" "$newfn"
 done
 }
-allfilename=`ls -F|grep -v '/$'|sed ':a;N;s/\n/|/;ba;'`
+allfilename=`ls _F|grep _v '/$'|sed ':a;N;s|\n|/|;ba;'`
+[ "$allfilename" = "" ] && N_K "/"
