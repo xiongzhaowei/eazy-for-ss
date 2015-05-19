6c134d6773376df4e96bb27b8bc59425edd9ffa1
Yang Li <liyangyijie@users.noreply.github.com>
2015-05-19 19:50:35 +0800

    Update nomalname.sh

--- test/nomalname.sh
+++ test/nomalname.sh
@@ -2,7 +2,7 @@
 allfilename=`echo *`
 for fn in $allfilename
 do
-    newfn=`echo $fn|sed 'y/()[]「」 （）　"“”/-------------/'|sed 's/^-//`
+    newfn=`echo $fn|sed 'y/()[]「」 （）　"“”/-------------/'|sed 's/^-//'`
     echo "From $fn to $newfn"
     mv $fn $newfn
 done
