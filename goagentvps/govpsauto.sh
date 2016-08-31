6a8ace94343f196d5faa4d9bfb005086b5967f27
Yang Li <liyangyijie@users.noreply.github.com>
2016-08-31 22:58:23 +0800

    acme;fix

--- goagentvps/govpsauto.sh
+++ goagentvps/govpsauto.sh
@@ -6,40 +6,32 @@ UserName=
 PassWord=
 Set_Port=
 #########################################################
-User_PATH=$(cd ~ ; echo `pwd`)
-Mycer_PATH="${User_PATH}/.le/${My_Domain}/${My_Domain}.cer"
-Mykey_PATH="${User_PATH}/.le/${My_Domain}/${My_Domain}.key"
+Mycer_PATH="${HOME}/.acme.sh/${My_Domain}/fullchain.cer"
+Mykey_PATH="${HOME}/.acme.sh/${My_Domain}/${My_Domain}.key"
 Port=${Set_Port:-443}
 #install dep
 apt-get install --no-install-recommends -y wget cron netcat curl sudo openssl \
 ca-certificates bash-completion git supervisor
 #install le
-cd ~
-git clone https://github.com/Neilpang/le.git
-cd le
-chmod +x le.sh
-./le.sh install
-sleep 1
-./le.sh install
+wget - https://get.acme.sh | sh
 . ~/.profile
-cd ..
-rm -rf le
-#get cer
-~/.le/le.sh issue no ${My_Domain}
+#get cer(The tcp 443 port MUST be free to listen)
+acme.sh --issue --tls -d ${My_Domain}
 # get govps
-wget --no-check-certificate -c https://github.com/fanyueciyuan/eazy-for-ss/raw/master/goagentvps/govpsx64.tar.xz -O /opt/govpsx64.tar.xz
+GV=`curl -sL https://github.com/phuslu/goproxy/releases |sed -n 's/.*<t.*vps.*-\([^<]*\)\.t.*/\1/p'`
+wget --no-check-certificate -c https://github.com/phuslu/goproxy/releases/download/goproxy/goproxy-vps_linux_amd64-${GV}.tar.bz2 -O /opt/govpsx64.tar.bz2
 cd /opt
-tar xf govpsx64.tar.xz
+tar xf govpsx64.tar.bz2
 chmod +x govps
 #daemon by supervisor
 cat > /etc/supervisor/conf.d/govps.conf<< EOF
 [program:govps]
-command=/opt/govps -addr=":${Port}" -auth="${UserName}:${PassWord}" -certFile="${Mycer_PATH}" -keyFile="${Mykey_PATH}"
+command=/opt/govps -addr=":${Port}" -auth="${UserName}:${PassWord}" -certfile="${Mycer_PATH}" -keyfile="${Mykey_PATH}"
 autostart=true
 autorestart=true
 user=root
 EOF
-
 /etc/init.d/supervisor stop
 sleep 1
 /etc/init.d/supervisor start
+supervisorctl reload
